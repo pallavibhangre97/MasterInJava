0e0993835980b215f5f9555d4bf191c511e2c87c
pallavichinchwade <pallavibhangre97@gmail.com>
2020-06-11 16:51:20 +0530

    Simple inheritance problem

---
+++ src/com/javalearning/inheritance/simple/Employee.java
@@ -0,0 +1,6 @@
+package com.javalearning.inheritance;
+
+public class Employee{
+    int id=11;
+    int sal=20000;
+}
---
+++ src/com/javalearning/inheritance/simple/SimpleInheritance.java
@@ -0,0 +1,11 @@
+package com.javalearning.inheritance;
+
+public class SimpleInheritance {
+    public static void main(String[] args) {
+        Programmer p=new Programmer();
+
+        System.out.println(p.id);
+        System.out.println(p.sal);
+        System.out.println(p.name);
+    }
+}
